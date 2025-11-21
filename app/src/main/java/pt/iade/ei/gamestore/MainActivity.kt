c95d0b43bdfb857f008a74688cf80a97f7c229a3
TPereira324 <tahawurpereira1@gmail.com>
2025-11-21 12:33:46 +0000

    criação do gamedetails e alterção da navigation de navhost para intent

--- app/src/main/java/pt/iade/ei/gamestore/MainActivity.kt
+++ app/src/main/java/pt/iade/ei/gamestore/MainActivity.kt
@@ -1,14 +1,26 @@
 package pt.iade.ei.gamestore
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import pt.iade.ei.gamestore.controller.StoreViewModel
+import pt.iade.ei.gamestore.model.Game
 import pt.iade.ei.gamestore.ui.theme.GameStoreTheme
-import pt.iade.ei.gamestore.view.MainNavigation
+import pt.iade.ei.gamestore.view.TelaPrincipalScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { GameStoreTheme { MainNavigation() } }
+        val store = StoreViewModel()
+        setContent {
+            GameStoreTheme {
+                TelaPrincipalScreen(games = store.games, onGameClick = { game: Game ->
+                    val intent = Intent(this, pt.iade.ei.gamestore.view.GameDetailActivity::class.java)
+                    intent.putExtra("game", game)
+                    startActivity(intent)
+                })
+            }
+        }
     }
 }
